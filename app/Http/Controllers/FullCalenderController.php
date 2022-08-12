91603c4fabcc656d7b299ce86220983c56682c0d
wassim lahlali <wlahlali343@gmail.com>
2022-08-12 12:21:52 +0100

    ajouter la methode des actions qui pourrant s efectuer par l utulisateur comme l ajout d un evenement et sa supression

--- app/Http/Controllers/FullCalenderController.php
+++ app/Http/Controllers/FullCalenderController.php
@@ -18,4 +18,38 @@ class FullCalenderController extends Controller
     	}
     	return view('full-calender');
     }
+    public function action(Request $request)
+    {
+    	if($request->ajax())
+    	{
+    		if($request->type == 'add')
+    		{
+    			$event = Event::create([
+    				'title'		=>	$request->title,
+    				'start'		=>	$request->start,
+    				'end'		=>	$request->end
+    			]);
+
+    			return response()->json($event);
+    		}
+
+    		if($request->type == 'update')
+    		{
+    			$event = Event::find($request->id)->update([
+    				'title'		=>	$request->title,
+    				'start'		=>	$request->start,
+    				'end'		=>	$request->end
+    			]);
+
+    			return response()->json($event);
+    		}
+
+    		if($request->type == 'delete')
+    		{
+    			$event = Event::find($request->id)->delete();
+
+    			return response()->json($event);
+    		}
+    	}
+    }
 }
