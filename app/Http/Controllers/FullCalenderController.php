101ce37d493e246cdf9130a9c3687545e50ad48e
wassim lahlali <wlahlali343@gmail.com>
2022-08-12 12:11:23 +0100

    ajouter la methode index qui traite les infoos et rederige vers le fichier full-callender dans le dossier view

--- app/Http/Controllers/FullCalenderController.php
+++ app/Http/Controllers/FullCalenderController.php
@@ -7,4 +7,15 @@ use Illuminate\Http\Request;
 class FullCalenderController extends Controller
 {
     //
+    public function index(Request $request)
+    {
+    	if($request->ajax())
+    	{
+    		$data = Event::whereDate('start', '>=', $request->start)
+                       ->whereDate('end',   '<=', $request->end)
+                       ->get(['id', 'title', 'start', 'end']);
+            return response()->json($data);
+    	}
+    	return view('full-calender');
+    }
 }
